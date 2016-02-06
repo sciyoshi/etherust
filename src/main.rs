e0990a01297699775e31c829fa050dab9b8cc95f
Samuel Cormier-Iijima <sciyoshi@gmail.com>
2016-02-06 17:59:04 -0500

    mio

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,102 @@
-extern crate secp256k1;
+extern crate mio;
 extern crate rand;
-extern crate byteorder;
-extern crate crypto;
 extern crate etherust;
+extern crate secp256k1;
 
+use std::collections::HashMap;
 use std::io::prelude::*;
-use std::time;
-use std::thread;
-use rand::Rng;
-use std::net::TcpStream;
-use byteorder::{BigEndian, ByteOrder};
-use crypto::mac::Mac;
-use crypto::digest::Digest;
-use crypto::symmetriccipher::{Encryptor, Decryptor};
-use crypto::{sha2, sha3, aes, mac, hmac};
+use rand::thread_rng;
+
+use mio::{EventLoop, Handler, Token, EventSet, PollOpt};
+use mio::tcp::{TcpStream};
 
 use etherust::rlpx::RlpxContext;
-use etherust::crypto::{KeyDerivation, EncryptionContext};
+use etherust::crypto::EncryptionContext;
+
+#[derive(Debug)]
+enum PeerState {
+	Initializing
+}
+
+#[derive(Debug)]
+struct Peer {
+	pub stream: TcpStream,
+	pub state: PeerState
+}
+
+impl Peer {
+	pub fn new(stream: TcpStream) -> Self {
+		Peer {
+			stream: stream,
+			state: PeerState::Initializing
+		}
+	}
+}
+
+struct PeerHandler {
+	pub counter: usize,
+	pub peers: HashMap<Token, Peer>
+}
+
+impl PeerHandler {
+	pub fn new() -> Self {
+		PeerHandler {
+			counter: 0,
+			peers: HashMap::new()
+		}
+	}
+
+	pub fn register_peer(&mut self, event_loop: &mut EventLoop<Self>, stream: TcpStream) {
+		let peer = Peer::new(stream);
+		let token = Token(self.counter);
+
+		self.counter += 1;
+
+		event_loop.register(&peer.stream, token, EventSet::all(), PollOpt::edge()).unwrap();
+
+		self.peers.insert(token, peer);
+	}
+}
+
+impl Handler for PeerHandler {
+	type Timeout = ();
+	type Message = u32;
+
+	fn ready(&mut self, event_loop: &mut EventLoop<PeerHandler>, token: Token, events: EventSet) {
+		match self.peers.get_mut(&token) {
+			Some(peer) => {
+				println!("ready!, {:?} {:?}", events, peer);
+			},
+			None => {
+				panic!("received event for non-existent peer");
+			}
+		}
+	}
+}
 
 fn main() {
-	let mut rng = rand::OsRng::new().unwrap();
 	let secp = secp256k1::Secp256k1::new();
-	let mut hasher = sha3::Sha3::keccak256();
+	let privkey = secp256k1::key::SecretKey::new(&secp, &mut thread_rng());
 
-	let token = 0u8;
+	let rpub = b"\x04E\xae\xfc\xd8\xb4iQR]\xf9\x1c\x9b\xb1@\x89\xb6\x99%2_\xc5\x8e\\mb\xd6\xdb\x85O\x9b\x93\x99\xd3\xc0\x9b\x96p5\x90n\x05\xe3\xca\xd6\x15\xca\x93\x05\xa6u\x8c\x90A|\xc9\xca\xe3\xc6\x87\xaf=n]A";
 
-	//let rpub = b"\x04\xa9y\xfbWT\x95\xb8\xd6\xdbD\xf7P1}\x0fF\"\xbfL*\xa36]j\xf7\xc2\x843\x99h\xee\xf2\x9bi\xad\r\xcer\xa4\xd8\xdb^\xbbIh\xde\x0e;\xec\x91\x01\'\xf14w\x9f\xbc\xb0\xcbm31\x16<";
 	//let rpub = b"\x04\xa7J6\xc3\xf9\xe9\x82>\xe7\x00\xd8XF\x90\x04@]7\xe2FY\xea\xc8\x13\xbac?\xbfo\x9e\xdf\x14;\xb3\x90^ \x94\xb7\xbb\xd2\xad\xb5s\xe7\xeds[\x87\xe8>+\x13\x86\xd32)\x9e+\x11\xb1\x91\x85T";
-	//let rpub = b"\x04E\xae\xfc\xd8\xb4iQR]\xf9\x1c\x9b\xb1@\x89\xb6\x99%2_\xc5\x8e\\mb\xd6\xdb\x85O\x9b\x93\x99\xd3\xc0\x9b\x96p5\x90n\x05\xe3\xca\xd6\x15\xca\x93\x05\xa6u\x8c\x90A|\xc9\xca\xe3\xc6\x87\xaf=n]A";
-	let rpub = b"\x04\xa7J6\xc3\xf9\xe9\x82>\xe7\x00\xd8XF\x90\x04@]7\xe2FY\xea\xc8\x13\xbac?\xbfo\x9e\xdf\x14;\xb3\x90^ \x94\xb7\xbb\xd2\xad\xb5s\xe7\xeds[\x87\xe8>+\x13\x86\xd32)\x9e+\x11\xb1\x91\x85T";
+
 	let rpubkey = secp256k1::key::PublicKey::from_slice(&secp, rpub).unwrap();
 
-	let (privkey, pubkey) = secp.generate_keypair(&mut rng).unwrap();
+	//////////////////////////////////
 
+	let mut event_loop = EventLoop::new().unwrap();
 
+	let mut peer_handler = PeerHandler::new();
 
+	let stream = TcpStream::connect(&"127.0.0.1:30303".parse().unwrap()).unwrap();
+
+	peer_handler.register_peer(&mut event_loop, stream);
+
+	let _ = event_loop.run(&mut peer_handler);
+
+	/*
 	let ctx = EncryptionContext {
 		curve: &secp,
 		privkey: &privkey,
@@ -45,50 +107,15 @@ fn main() {
 
 	let encrypted = rlpx.handshake();
 
-	let mut stream = TcpStream::connect("127.0.0.1:30303").unwrap();
-
 	let mut read = [0u8; 210];
 
 	let writeres = stream.write(&encrypted);
 
 	let readres = stream.read(&mut read);
 
-	println!("RES: {:?}", writeres);
-	println!("RES: {:?}", readres);
-	println!("READ: {:?}", &read[..]);
+	println!("WROTE: {:?}", writeres);
+	println!("READ: {:?}", readres);
 
-	let authresp = ctx.decrypt(&read[..]);
-
-	let mut key = vec![4u8];
-
-	key.extend_from_slice(&authresp[..64]);
-
-	let remoterandpubkey = secp256k1::key::PublicKey::from_slice(&secp, &key).unwrap();
-
-	let remotenonce = &authresp[64..96];
-	let remotetoken = &authresp[96];
-
-	println!("REMOTERANDPUBKEY: {:?}", &remoterandpubkey.serialize_vec(&secp, false));
-
-	let ecdheSecret = secp256k1::ecdh::SharedSecret::new_raw(&secp, &remoterandpubkey, &rlpx.randprivkey);
-	let mut sharedSecret = [0u8; 32];
-
-	let mut sharedTmp = [0u8; 32];
-
-	println!("RNONCE\n========\n{:?}", &remotenonce);
-
-	hasher.input(&remotenonce);
-	hasher.input(&rlpx.nonce);
-	hasher.result(&mut sharedTmp);
-	hasher.reset();
-
-	println!("STMP\n========\n{:?}", &sharedTmp);
-
-	hasher.input(&ecdheSecret[..]);
-	hasher.input(&sharedTmp);
-	hasher.result(&mut sharedSecret);
-	hasher.reset();
-
-	println!("ecdheSecret: {:?}", &ecdheSecret[..]);
-	println!("sharedSecret: {:?}", &sharedSecret);
+	rlpx.auth_handshake_decode(&read);
+	*/
 }
--- src/rlpx.rs
+++ src/rlpx.rs
@@ -62,15 +62,44 @@ impl<'a> RlpxContext<'a> {
 		msg.extend_from_slice(&self.nonce);
 		msg.push(token);
 
-		println!("RANDPUBKEY\n========\n{:?}", &self.randpubkey.serialize_vec(self.context.curve, false)[1..]);
-		println!("RANDPUBSHA\n========\n{:?}", &randpubsha);
-		println!("SIGNED\n========\n{:?}", &signed);
-		println!("SHARED\n========\n{:?}", &shared);
-		println!("SIGSERIAL\n========\n{:?}", &sigserial[..]);
-		println!("SIGRECID\n========\n{:?}", sigrecid.to_i32() as u8);
-		println!("PUBKEY\n========\n{:?}", &self.pubkey.serialize_vec(self.context.curve, false)[1..]);
-		println!("NONCE\n========\n{:?}", &self.nonce);
-
 		self.context.encrypt(&msg)
+	}
+
+	pub fn auth_handshake_decode(&self, msg: &[u8]) {
+		let mut hasher = sha3::Sha3::keccak256();
+		let authresp = self.context.decrypt(msg);
+
+		let mut key = vec![4u8];
+
+		key.extend_from_slice(&authresp[..64]);
+
+		let remoterandpubkey = secp256k1::key::PublicKey::from_slice(self.context.curve, &key).unwrap();
+
+		let remotenonce = &authresp[64..96];
+		let remotetoken = &authresp[96];
+
+		println!("REMOTERANDPUBKEY: {:?}", &remoterandpubkey.serialize_vec(self.context.curve, false));
+
+		let ecdheSecret = secp256k1::ecdh::SharedSecret::new_raw(self.context.curve, &remoterandpubkey, &self.randprivkey);
+		let mut sharedSecret = [0u8; 32];
+
+		let mut sharedTmp = [0u8; 32];
+
+		println!("RNONCE\n========\n{:?}", &remotenonce);
+
+		hasher.input(&remotenonce);
+		hasher.input(&self.nonce);
+		hasher.result(&mut sharedTmp);
+		hasher.reset();
+
+		println!("STMP\n========\n{:?}", &sharedTmp);
+
+		hasher.input(&ecdheSecret[..]);
+		hasher.input(&sharedTmp);
+		hasher.result(&mut sharedSecret);
+		hasher.reset();
+
+		println!("ecdheSecret: {:?}", &ecdheSecret[..]);
+		println!("sharedSecret: {:?}", &sharedSecret);
 	}
 }
